29f0e0539bf4be9f989e700dd2fdf8b2b47ef4a5
Ricardo Fulgencio Alves <rfulgencio3@gmail.com>
2021-12-07 18:53:35 -0300

    Add project files.

---
+++ src/KafkaSample.Producer/Program.cs
@@ -0,0 +1,37 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KafkaSample.Producer
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var count = 0;
+                    while(count <= 100)
+                    {
+                        var dr = await p.ProduceAsync("test-publish-topic", 
+                            new Message<Null, string> { Value = $"publish-test: {count}" });
+
+                        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                        count++;
+
+                        Thread.Sleep(2000);
+                    }
+                }
+                catch (ProduceException<Null, string> ex)
+                {
+                    Console.WriteLine($"Delivery failed: {ex.Error.Reason}");
+                }
+            }
+        }
+    }
+}
